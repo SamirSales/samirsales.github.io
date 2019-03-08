e8cd452bab97aa0a9645724d4b7f2c1a2b98a2ee
SamirSales <samir.sribeiro@gmail.com>
2019-03-07 21:53:53 -0300

    selecting terms to chart

--- skills_chart.js
+++ skills_chart.js
@@ -5,26 +5,30 @@ var experiences_length = app._data.experiences.length;
 var tech_dictionary = {};
 var backgroundColor = ['#002535', '#003851', '#22646e', 'bfd2d9', '#3b89ac', '#003525', '#005138', '#226e64', '#bfd9d2', '#3bac89','#252035', '#382051'];
 
+
+function setTechDictionary(projects){
+    for(j=0; j < projects.length; j++){
+        project = projects[j];
+
+        for(n=0; n < project.technologies.length; n++){
+            tech = project.technologies[n];
+
+            if(!tech_dictionary[tech]){
+                tech_dictionary[tech] = 1;
+            }else{
+                tech_dictionary[tech] += 1;
+            }
+        }
+    }
+}
+
 function resetTechDictionary(){
     tech_dictionary = {};
 
     for(i = 0; i < experiences_length; i++){
-        experience = app._data.experiences[i];
-    
-        for(j=0; j < experience.projects.length; j++){
-            project = experience.projects[j];
-    
-            for(n=0; n < project.technologies.length; n++){
-                tech = project.technologies[n];
-    
-                if(!tech_dictionary[tech]){
-                    tech_dictionary[tech] = 1;
-                }else{
-                    tech_dictionary[tech] += 1;
-                }
-            }
-        }
+        setTechDictionary(app._data.experiences[i].projects);
     }
+    setTechDictionary(app._data.personalProjects);
 }
 
 var arrayLabelTech = [];
@@ -32,22 +36,34 @@ var arrayValueTech = [];
 var someTech;
 var TECH_TO_CHART = 12;
 
+function addTermToChart(term){
+    arrayValueTech.push(tech_dictionary[term]); 
+    arrayLabelTech.push(term); 
+}
+
 function resetTechArrayOfLabelAndValue(){
     arrayLabelTech = [];
     arrayValueTech = [];
 
     for(i=0; i < TECH_TO_CHART; i++){
         someTech = Object.keys(tech_dictionary).reduce(function(a, b){ return tech_dictionary[a] > tech_dictionary[b] ? a : b });
-        arrayValueTech.push(tech_dictionary[someTech]); 
-        arrayLabelTech.push(someTech); 
+        addTermToChart(someTech);
         delete tech_dictionary[someTech];  
     }
 }
 
 resetTechDictionary();
-resetTechArrayOfLabelAndValue();
+// resetTechArrayOfLabelAndValue();
 
-console.log('tech_dictionary', tech_dictionary);
+addTermToChart('Java');
+addTermToChart('Android');
+addTermToChart('SQLite');
+addTermToChart('SpringBoot');
+addTermToChart('ReactJS');
+addTermToChart('Angular');
+addTermToChart('AngularJS');
+addTermToChart('PostgreeSQL');
+addTermToChart('iOS');      
 
 var data = {
     datasets: [{
